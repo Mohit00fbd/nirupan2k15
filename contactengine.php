d630e17a63d28df35c7d2a5cbbf27e1fd9ededf5
Mradul Verma <mohit00fbd@gmail.com>
2015-06-20 07:34:04 +0530

    Nirupan Website for 2015
    NIRUPAN is cultural-cum-sports event hosted by students of Dr. S.
    Radhakrishnan Hostel, in which students of all the major colleges
    participate. NIRUPAN is an event which gives you a platform to show your
    skills to a large community of students.

---
+++ contactengine.php
@@ -0,0 +1,35 @@
+<?php
+
+$EmailFrom = "quickcontact@nirupan.webege.com";
+$EmailTo = "nirupan2k15@gmail.com";
+$Subject = "Quick Contact Website Entry";
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contactthanks.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Message Sent</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<img src="images/logo.png" alt="Nirupan" />
+			<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="index.html">Back to Home Page</a></p>
+	
+	</div>
+</body>
+
+</html>
